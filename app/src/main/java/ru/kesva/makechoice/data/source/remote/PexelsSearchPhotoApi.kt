fdd828850eb5330571ae29658694abf5de552074
maria-93 <55946611+maria-93@users.noreply.github.com>
2021-03-02 18:17:25 +0300

    The interface for the Pexels API is completed.

--- app/src/main/java/ru/kesva/makechoice/data/source/remote/PexelsSearchPhotoApi.kt
+++ app/src/main/java/ru/kesva/makechoice/data/source/remote/PexelsSearchPhotoApi.kt
@@ -2,16 +2,18 @@ package ru.kesva.makechoice.data.source.remote
 
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
-import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Query
+import ru.kesva.makechoice.data.model.PexelsResponse
 
 interface PexelsSearchPhotoApi {
     companion object {
-        private const val apiBaseUrl = "https://api.pexels.com/v1/search"
+        private const val apiBaseUrl = "https://api.pexels.com/"
         fun create(): PexelsSearchPhotoApi {
             val logging = HttpLoggingInterceptor().apply { level = HttpLoggingInterceptor.Level.BASIC }
             val httpClient = OkHttpClient.Builder()
@@ -29,5 +31,11 @@ interface PexelsSearchPhotoApi {
         }
     }
 
+    @GET("v1/search")
+    fun getPhotoResponse(
+        @Header("Authorization") key: String,
+        @Query("locale") locale: String,
+        @Query("query") query: String
+    ): PexelsResponse
 
 }
